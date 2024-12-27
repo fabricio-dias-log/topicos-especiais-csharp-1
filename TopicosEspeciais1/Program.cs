e6b079ac0fd18c21e30173465967aada5374b5e5
Fabricio Dias <fabricio270909@gmail.com>
2024-12-27 20:10:57 -0300

    Tópico 4: Funções para string

--- TopicosEspeciais1/Program.cs
+++ TopicosEspeciais1/Program.cs
@@ -58,5 +58,33 @@ class Program
         desconto = (preco < 20.00) ? preco * 0.1 : preco * 0.05;
 
         Console.WriteLine($"Desconto: {desconto.ToString("F2", CultureInfo.InvariantCulture)}");
+        
+        //Funções para string
+
+        string original = "abcde FGHIJ ABC abc DEFG     ";
+        string s1 = original.ToUpper();
+        string s2 = original.ToLower();
+        string s3 = original.Trim();
+        int n1 = original.IndexOf("bc");
+        int n2 = original.LastIndexOf("bc");
+        string s4 = original.Substring(3);
+        string s5 = original.Substring(3, 5);
+        string s6 = original.Replace('a', 'x');
+        string s7 = original.Replace("abc", "xy");
+        bool b1 = String.IsNullOrEmpty(original);
+        bool b2 = String.IsNullOrWhiteSpace(original);
+        
+        Console.WriteLine($"Original: -{original}-");
+        Console.WriteLine($"ToUpper: -{s1}-");
+        Console.WriteLine($"ToLower: -{s2}-");
+        Console.WriteLine($"Trim: -{s3}-");
+        Console.WriteLine($"IndexOf: -{n1}-");
+        Console.WriteLine($"LastIndexOf: -{n2}-");
+        Console.WriteLine($"Substring(3): -{s4}-");
+        Console.WriteLine($"Substring(3, 5): -{s5}-");
+        Console.WriteLine($"Replace('a', 'x'): -{s6}-");
+        Console.WriteLine($"Replace('abc', 'xy'): -{s7}-");
+        Console.WriteLine($"String.IsNullOrEmpty: -{b1}-");
+        Console.WriteLine($"String.IsNullOrWhiteSpace: -{b2}-");
     }
 }
