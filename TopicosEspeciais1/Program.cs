1bfb23b8ba2cf5ba67640707beb9947018fef250
Fabricio Dias <fabricio270909@gmail.com>
2024-12-27 19:36:49 -0300

    Tópico 1: Inferência

--- TopicosEspeciais1/Program.cs
+++ TopicosEspeciais1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //inferencia: palavra var
+
+        var x = 10;
+        var y = 10.1;
+        var z = "Maria";
+
+        Console.WriteLine(x);
+        Console.WriteLine(y);
+        Console.WriteLine(z);
     }
 }
