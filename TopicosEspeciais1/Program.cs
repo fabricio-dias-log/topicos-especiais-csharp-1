c087123c62e37fdfe5dd44c325c9550c27d60771
Fabricio Dias <fabricio270909@gmail.com>
2024-12-27 19:41:20 -0300

    Tópico 2: Switch-Case

--- TopicosEspeciais1/Program.cs
+++ TopicosEspeciais1/Program.cs
@@ -13,5 +13,38 @@ class Program
         Console.WriteLine(x);
         Console.WriteLine(y);
         Console.WriteLine(z);
+        
+        // switch-case
+        int n = int.Parse(Console.ReadLine());
+        string day;
+
+        switch (n)
+        {
+            case 1:
+                day = "Monday";
+                break;
+            case 2:
+                day = "Tuesday";
+                break;
+            case 3:
+                day = "Wednesday";
+                break;
+            case 4:
+                day = "Thursday";
+                break;
+            case 5:
+                day = "Friday";
+                break;
+            case 6:
+                day = "Saturday";
+                break;
+            case 7:
+                day = "Sunday";
+                break;
+            default:
+                day = "Invalid value";
+                break;
+        }
+        Console.WriteLine($"Dia: {day}");
     }
 }
